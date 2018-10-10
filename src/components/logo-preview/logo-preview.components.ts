bc6d66c506797ec69707cdf3ac60f5b88ff89ebc
Fredy Talero <fredy.talero@gmail.com>
2018-10-09 19:32:46 -0500

    Inicio

---
+++ src/components/logo-preview/logo-preview.components.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'logo-preview',
+    templateUrl: './logo-preview.component.html',
+    styleUrls: ['./logo-preview.component.scss']
+})
+
+export class LogoPreviewComponent{}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,4 +11,9 @@ export class HomePage {
 
   }
 
+  mostrarNombre():void{
+    console.log("Fredy Talero");
+  }
+
+
 }
